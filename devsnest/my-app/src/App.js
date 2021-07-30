af4243dbc74c5e9ae2c9ea2e9e75f5015b86f1f0
Jitender Chaudhary <85493949+codvik@users.noreply.github.com>
2021-07-30 21:59:20 +0530

    Add files via upload

---
+++ devsnest/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="https://tsh.io/wp-content/uploads/2019/12/react-meme1_.png" alt="Meme" className="center"/>
+      React Card
+    </div>
+  );
+}
+
+export default App;
